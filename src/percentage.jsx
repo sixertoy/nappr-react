462a992555ee3c39b4d6acc833c71f9bb7861b4e
sixertoy <sixertoy.github@gmail.com>
2019-12-03 18:51:42 +0100

    add prop-types peer dependencies

--- src/percentage.jsx
+++ src/percentage.jsx
@@ -1,9 +1,15 @@
 import { getPercentage } from '@iziges/napper-core/lib/maths';
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const Percentage = (percent, value) => {
+const Percentage = ({ percent, value }) => {
   const result = getPercentage(percent, value);
   return <span>{result}&nbsp;%</span>;
+};
+
+Percentage.propTypes = {
+  percent: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
 export default Percentage;
