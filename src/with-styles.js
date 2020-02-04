17262d2adc7d7ac22aafc4c7c250ff2e132eef73
sixertoy <sixertoy.github@gmail.com>
2020-02-04 08:44:29 +0100

    update

--- src/with-styles.js
+++ src/with-styles.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import withSizes from 'react-sizes';
+import { compose } from 'redux';
+import { withTheme } from 'theming';
+
+const withSizesHOC = withSizes(({ height, width }) => ({
+  height,
+  isMobile: width <= 960,
+  width,
+}));
 
 export const withStyles = (styles = {}) => {
   const useStyles = createUseStyles(styles);
-
   return WrappedComponent => {
     const StyledComponent = props => {
       const classes = useStyles({ ...props });
       return <WrappedComponent {...props} classes={classes} />;
     };
 
-    const mapSizeToProps = ({ height, width }) => ({
-      height,
-      isMobile: width <= 960,
-      width,
-    });
-
-    return withSizes(mapSizeToProps)(StyledComponent);
+    return compose(withSizesHOC, withTheme)(StyledComponent);
   };
 };
 
