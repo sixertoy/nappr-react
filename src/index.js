7d5aecb6a7377f06d21fb415a92b3068d0198886
sixertoy <sixertoy.github@gmail.com>
2020-03-09 03:18:42 +0100

    :trash: remove react-sizes from withStyles HOC

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 export { default as withStyles } from './with-styles';
-export { default as withTheme } from './with-theme';
 export { default as Percentage } from './percentage';
--- src/with-styles.js
+++ src/with-styles.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
-import withSizes from 'react-sizes';
-import { compose } from 'redux';
 import { withTheme } from 'theming';
-
-const withSizesHOC = withSizes(({ height, width }) => ({
-  height,
-  isMobile: width <= 960,
-  width,
-}));
 
 export const withStyles = (styles = {}) => {
   const useStyles = createUseStyles(styles);
@@ -19,7 +11,7 @@ export const withStyles = (styles = {}) => {
       return <WrappedComponent {...props} classes={classes} />;
     };
 
-    return compose(withSizesHOC, withTheme)(StyledComponent);
+    return withTheme(StyledComponent);
   };
 };
 
